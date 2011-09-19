e4414ada64fb158e1fe4682f4b002ae9a1a768b6
Pavel Gabriel <alovak@gmail.com>
2011-09-19 11:50:17 +0300

    date for posts was added

--- lib/default.rb
+++ lib/default.rb
@@ -1,9 +1,18 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+require "rubygems"
+require "bundler/setup"
+
 require 'compass'
+require 'chronic'
 
 Compass.add_project_configuration('compass/config.rb')
 
 include Nanoc3::Helpers::Blogging
 
 Nanoc3::Filter.register '::Nanoc3::Filters::Sass', :scss
+
+def date_of(item)
+  date = Chronic.parse(item[:created_at])
+  date.strftime('%d %b %Y')
+end
